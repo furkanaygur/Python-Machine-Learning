da5e28bffd55f02ee10b07d0f1f052c9a619fa55
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-11-04 00:16:28 +0300

    Results

--- R2 Values/Example/r2Example.py
+++ R2 Values/Example/r2Example.py
@@ -4,6 +4,35 @@
 @author: furkan
 """
 
+'''
+3 paramteres results
+
+linear R-squared: 0.903
+
+Poşynomial R-squared: 0.680
+
+SVR R-squared: 0.782
+
+DT R-squared: 0.679
+
+RF R-squared: 0.713
+
+
+1 parameter results
+
+linear R-squared: 0.942
+
+Polynomial R-squared: 0.759
+
+SVR R-squared: 0.770
+
+DT R-squared: 0.751
+
+RF R-squared: 0.719
+
+'''
+
+
 import pandas as pd 
 from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
@@ -11,10 +40,14 @@ from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
 
 datas = pd.read_csv("newSalaries.csv")
 
-x = datas.iloc[:,2:3].values
+print(datas.corr())
+
+x = datas.iloc[:,2:5].values
 y = datas.iloc[:,5:].values
  
 # Linear Regression
@@ -25,6 +58,7 @@ model = sm.OLS(lr.predict(x) ,x).fit()
 print(" Linear OLS ".center(50,'*'))
 print(model.summary())
 
+print(" \nLinear Regression R2 Value: ", r2_score(y,lr.predict(x)),'\n')
 
 # Polynomial Regression
 pr = PolynomialFeatures(degree=4)
@@ -36,6 +70,8 @@ lr2.fit(x_poly, y)
 model2 = sm.OLS( lr2.predict(x_poly), x).fit()
 print(" Polynomial OLS ".center(50,'*'))
 print(model2.summary())
+
+print("\nPolynomial Regression R2 Value: ", r2_score(y, lr2.predict(x_poly)),'\n')
 
 # SVR 
 sc = StandardScaler()
@@ -49,6 +85,8 @@ model3 = sm.OLS(svr_reg.predict(x_scaled),x_scaled).fit()
 print(" SVR OLS ".center(50,'*'))
 print(model3.summary())
 
+print("\nSVR R2 Value: ", r2_score(y_scaled, svr_reg.predict(x_scaled)),'\n')
+
 
 # Decision Tree
 dt = DecisionTreeRegressor(random_state=0)
@@ -58,6 +96,8 @@ model4 = sm.OLS(dt.predict(x), x).fit()
 print(' Decision Tree OLS '.center(50,'*'))
 print(model4.summary())
 
+print("\n Decision Tree R2 Value: ", r2_score(y, dt.predict(x)),'\n') 
+
 # Random Forest 
 rf = RandomForestRegressor(n_estimators=10, random_state=0)
 rf.fit(x,y)
@@ -66,7 +106,7 @@ model5 = sm.OLS(rf.predict(x),x).fit()
 print(' Random Forest OLS '.center(50,'*'))
 print(model5.summary())
 
-
+print("\nRandom Forest R2 value: ",r2_score(y, rf.predict(x)),'\n')
 
 
 
