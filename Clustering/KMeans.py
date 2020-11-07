3914e69e961943f29eee4c9f360057c1b742ba53
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-11-08 00:38:04 +0300

    K-Means

--- Clustering/KMeans.py
+++ Clustering/KMeans.py
@@ -12,10 +12,10 @@ datas = pd.read_csv("customers.csv")
 
 x = datas.iloc[:,3:].values
 
-kmenas = KMeans(n_clusters=3, init='k-means++')
-kmenas.fit(x)
+kmeans = KMeans(n_clusters=3, init='k-means++')
+kmeans.fit(x)
 
-print(kmenas.cluster_centers_)
+print(kmeans.cluster_centers_)
 
 results = []
 
@@ -25,3 +25,15 @@ for i in range(1,11):
     results.append(kmeans.inertia_)
     
 plt.plot(range(1,11), results)
+plt.show()
+
+
+
+kmeans2 = KMeans(n_clusters=4, init='k-means++', random_state=123 )
+y_pred = kmeans2.fit_predict(x)
+
+print(y_pred)
+
+plt.scatter(x[y_pred == 0,0], x[y_pred == 0,1],s=100, c='red')
+plt.scatter(x[y_pred == 1,0], x[y_pred == 1,1],s=100, c='green')
+plt.scatter(x[y_pred == 2,0], x[y_pred == 2,1],s=100, c='blue')
