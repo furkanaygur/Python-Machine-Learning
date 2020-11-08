310cf416a34bd07afb61ca9f7773092dab367a4a
Furkan Aygur <32839776+furkanaygur@users.noreply.github.com>
2020-11-08 21:44:48 +0300

    Apriori

---
+++ Association Rule Mining/Apriori.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: furkan
+"""
+
+import pandas as pd
+from libraries.apyori import apriori
+
+datas = pd.read_csv('cart.csv',header=None)
+
+x = []
+
+for i in range(0,7501):
+    x.append( [str(datas.values[i,j]) for j in range(0,19)] )
+
+rules = apriori(x,min_support=0.01, min_confidence=0.2, min_lift=3, min_lenght=2 )
+
+print(list(rules))
